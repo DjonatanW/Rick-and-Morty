e70158eb108d45b0e44a119508f7ba39f6bed7db
Djonatan Weber <djonatan11021996@gmail.com>
2024-11-24 18:26:50 -0300

    feat:ajust the project.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,73 +1,67 @@
 let pagina = 1
 let quantidadeDePaginas
-let contaQuantidadeDePaginas
 let count
-let statusPersonagen = ''
+let statusPersonagem = ''
 let morto
 let desconhecido
-let botaoPagina;
+let botaoPagina
 let result = []
 
-const cartoesPersonagensEL = document.getElementById("cards-personagens")
+const cartoesPersonagens = document.getElementById("cards-personagens")
 const containerPersonagens = document.getElementById("personagens")
 const containerbotoes = document.getElementById("botoes-pagina")
 
-//rota onde são carregadas as informações dos personagens
 async function carregarPersonagens() {
     try {
-        carregarLocalizacoes();
-        carregarEpisodios();
+        carregarLocalizacao()
+        carregarEpisodios()
 
-        const response = await api.get(`/character?page=${pagina}`);
-        const personagens = response.data.results;
-        console.log(response);
+        const response = await api.get(`/character?page=${pagina}`)
+        const personagens = response.data.results
+        console.log(response)
 
-        quantidadeDePaginas = response.data.info.pages;
+        quantidadeDePaginas = response.data.info.pages
 
-        limparElemento(cartoesPersonagensEL);
-        criaElementosPaginacao(quantidadeDePaginas)
+        limparElemento(cartoesPersonagens)
+        criarPaginacao(quantidadeDePaginas)
 
         personagens.forEach((personagem) => {
-            const cardElement = criarElementoCartao(personagem);
-            cartoesPersonagensEL.appendChild(cardElement);
-        });
+            const cardElement = criarCartao(personagem)
+            cartoesPersonagens.appendChild(cardElement)
+        })
 
-        mostraTotalPersonagens(response.data.info.count)
+        totalPersonagens(response.data.info.count)
 
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
 
-// paginação
 function incrementarPagina() {
     if (pagina !== quantidadeDePaginas) {
         pagina++
         containerbotoes.innerHTML = ''
         carregarPersonagens()
-        rolarTelaTopo()
+        telaSuperior()
     }
 }
 
-// paginação
 function outra(quantidadeDePaginas) {
     pagina = pagina - 2
-    criaElementosPaginacao(quantidadeDePaginas)
+    criarPaginacao(quantidadeDePaginas)
     carregarPersonagens()
 }
 
-// paginação
 function decrementarPagina(quantidadeDePaginas) {
     if (pagina > 1) {
         pagina--
         containerbotoes.innerHTML = ''
-        criaElementosPaginacao(quantidadeDePaginas)
+        criarPaginacao(quantidadeDePaginas)
         carregarPersonagens()
-        rolarTelaTopo()
+        telaSuperior()
     }
 }
 
-// paginação
 function botaoPosterior() {
     if (pagina == 1) {
         pagina = 3
@@ -77,12 +71,11 @@ function botaoPosterior() {
         pagina = 3
     }
     carregarPersonagens()
-    rolarTelaTopo()
+    telaSuperior()
 }
 
 
-//funcao para rolar a tela pro topo
-function rolarTelaTopo() {
+function telaSuperior() {
 
     window.scrollTo({
         top: 0,
@@ -90,40 +83,34 @@ function rolarTelaTopo() {
     })
 }
 
-async function carregamentoInicialPersonagens() {
+async function carregarOsPersonagens() {
     await carregarPersonagens()
 }
 
-// cria os botoes da paginação
-function criaElementosPaginacao(quantidadeDePaginas) {
-    console.log(pagina);
+function criarPaginacao(quantidadeDePaginas) {
+    console.log(pagina)
     containerbotoes.innerHTML = ''
 
-    // Seleciona o elemento pai
-    const navElemento = document.createElement("nav");
-    navElemento.setAttribute("aria-label", "Page navigation example");
+    const navElemento = document.createElement("nav")
+    navElemento.setAttribute("aria-label", "Page navigation example")
 
-    // Cria a lista de páginas
-    const ulElemento = document.createElement("ul");
-    ulElemento.className = "botao-paginacao";
+    const ulElemento = document.createElement("ul")
+    ulElemento.className = "botao-paginacao"
 
-    // Cria o botão "Anterior"
-    const liElementoAnterior = document.createElement("li");
-    liElementoAnterior.className = "page-item";
-    const aElementoAnterior = document.createElement("button");
+    const liElementoAnterior = document.createElement("li")
+    liElementoAnterior.className = "page-item"
+    const aElementoAnterior = document.createElement("button")
     aElementoAnterior.addEventListener('click', () => {
         decrementarPagina(quantidadeDePaginas)
     })
-    aElementoAnterior.className = "page-link";
-    aElementoAnterior.innerHTML = "Anterior";
-    liElementoAnterior.appendChild(aElementoAnterior);
-    ulElemento.appendChild(liElementoAnterior);
+    aElementoAnterior.className = "page-link"
+    aElementoAnterior.innerHTML = "Anterior"
+    liElementoAnterior.appendChild(aElementoAnterior)
+    ulElemento.appendChild(liElementoAnterior)
 
-    // Cria botões do meio
-    //Primeiro botão
-    const liElemento1 = document.createElement("li");
-    liElemento1.className = "page-item active";
-    const aElemento1 = document.createElement("button");
+    const liElemento1 = document.createElement("li")
+    liElemento1.className = "page-item active"
+    const aElemento1 = document.createElement("button")
     aElemento1.addEventListener('click', () => {
         if (pagina === quantidadeDePaginas) {
             outra(quantidadeDePaginas)
@@ -131,21 +118,20 @@ function criaElementosPaginacao(quantidadeDePaginas) {
         }
         decrementarPagina(quantidadeDePaginas)
     })
-    aElemento1.className = "page-link";
+    aElemento1.className = "page-link"
     if (pagina > 1) {
-        aElemento1.innerHTML = pagina - 1;
+        aElemento1.innerHTML = pagina - 1
     } else {
         aElemento1.innerHTML = 1
         liElementoAnterior.className = "disabled"
     }
 
-    liElemento1.appendChild(aElemento1);
-    ulElemento.appendChild(liElemento1);
+    liElemento1.appendChild(aElemento1)
+    ulElemento.appendChild(liElemento1)
 
-    //segundo botão
-    const liElemento2 = document.createElement("li");
-    liElemento2.className = "page-item";
-    const aElemento2 = document.createElement("button");
+    const liElemento2 = document.createElement("li")
+    liElemento2.className = "page-item"
+    const aElemento2 = document.createElement("button")
     aElemento2.addEventListener('click', () => {
         if (pagina === 1) {
             incrementarPagina()
@@ -154,28 +140,27 @@ function criaElementosPaginacao(quantidadeDePaginas) {
             decrementarPagina()
         }
     })
-    aElemento2.className = "page-link";
+    aElemento2.className = "page-link"
     if (pagina > 1) {
-        aElemento2.innerHTML = pagina;
+        aElemento2.innerHTML = pagina
         liElemento2.className = 'active'
         liElemento1.classList.remove('active')
     } else {
         aElemento2.innerHTML = 2
     }
 
-    liElemento2.appendChild(aElemento2);
-    ulElemento.appendChild(liElemento2);
+    liElemento2.appendChild(aElemento2)
+    ulElemento.appendChild(liElemento2)
 
-    //Terceiro botão
-    const liElemento3 = document.createElement("li");
-    liElemento3.className = "page-item";
-    const aElemento3 = document.createElement("button");
+    const liElemento3 = document.createElement("li")
+    liElemento3.className = "page-item"
+    const aElemento3 = document.createElement("button")
     aElemento3.addEventListener('click', () => {
         botaoPosterior()
     })
-    aElemento3.className = "page-link";
+    aElemento3.className = "page-link"
     if (pagina > 1) {
-        aElemento3.innerHTML = pagina + 1;
+        aElemento3.innerHTML = pagina + 1
     } else {
         aElemento3.innerHTML = 3
     }
@@ -186,13 +171,12 @@ function criaElementosPaginacao(quantidadeDePaginas) {
         liElemento3.className = 'active'
         liElemento2.classList.remove('active')
     }
-    liElemento3.appendChild(aElemento3);
-    ulElemento.appendChild(liElemento3);
+    liElemento3.appendChild(aElemento3)
+    ulElemento.appendChild(liElemento3)
 
-    // Cria o botão "Próxima"
-    const liProximoElemento = document.createElement("li");
-    liProximoElemento.className = "page-item";
-    const aProximoElemento = document.createElement("button");
+    const liProximoElemento = document.createElement("li")
+    liProximoElemento.className = "page-item"
+    const aProximoElemento = document.createElement("button")
     aProximoElemento.addEventListener('click', () => {
         incrementarPagina()
     })
@@ -200,79 +184,75 @@ function criaElementosPaginacao(quantidadeDePaginas) {
     if (pagina === quantidadeDePaginas) {
         liProximoElemento.className = "disabled"
     }
-    aProximoElemento.className = "page-link";
-    aProximoElemento.innerHTML = "Próxima";
-    liProximoElemento.appendChild(aProximoElemento);
-    ulElemento.appendChild(liProximoElemento);
+    aProximoElemento.className = "page-link"
+    aProximoElemento.innerHTML = "Próxima"
+    liProximoElemento.appendChild(aProximoElemento)
+    ulElemento.appendChild(liProximoElemento)
 
-    // Adiciona a lista de páginas ao elemento pai
-    navElemento.appendChild(ulElemento);
+    navElemento.appendChild(ulElemento)
     containerbotoes.appendChild(navElemento)
 }
 
-//cria os cards dos personagens
-function criarElementoCartao(personagem) {
-    //crio o elemento col
-    const colElemento = document.createElement('div');
-    colElemento.className = 'col-12 col-md-6 col-lg-4 my-3 d-flex justify-content-center';
+function criarCartao(personagem) {
+
+    const colElemento = document.createElement('div')
+    colElemento.className = 'col-12 col-md-6 col-lg-4 my-3 d-flex justify-content-center'
 
 
-    const cardElement = document.createElement('div');
-    cardElement.className = `tamanho-card puff-in-center bg-secondary text-white card my-2 mx-2 ${obterSombraStatusCard(personagem.status)}`;
-    const imageElement = document.createElement('img');
-    imageElement.src = personagem.image;
-    imageElement.className = 'card-img-top imagem-personagem';
-    imageElement.alt = 'Imagem do personagem';
+    const cardElement = document.createElement('div')
+    cardElement.className = `tamanho-card puff-in-center bg-secondary text-white card my-2 mx-2 ${sombraStatusCard(personagem.status)}`
+    const imageElement = document.createElement('img')
+    imageElement.src = personagem.image
+    imageElement.className = 'card-img-top imagem-personagem'
+    imageElement.alt = 'Imagem do personagem'
 
-    const cardBodyElement = document.createElement('div');
-    cardBodyElement.className = 'card-body background border border-top-0 border-success rounded-bottom';
+    const cardBodyElement = document.createElement('div')
+    cardBodyElement.className = 'card-body background border border-top-0 border-success rounded-bottom'
 
-    const titleElement = document.createElement('h6');
-    titleElement.className = 'card-title fs-5';
-    titleElement.textContent = personagem.name;
+    const titleElement = document.createElement('h6')
+    titleElement.className = 'card-title fs-5'
+    titleElement.textContent = personagem.name
 
-    const statusElement = document.createElement('p');
-    statusElement.className = 'card-text fs-6';
-    statusElement.innerHTML = obterIconeStatus(personagem.status);
+    const statusElement = document.createElement('p')
+    statusElement.className = 'card-text fs-6'
+    statusElement.innerHTML = iconeStatus(personagem.status)
 
-    const statusPersonagemElement = document.createElement('span');
-    statusPersonagemElement.className = 'fs-6 ms-3';
-    statusPersonagemElement.innerHTML = `${personagem.status} - ${personagem.species}`;
+    const statusPersonagemElement = document.createElement('span')
+    statusPersonagemElement.className = 'fs-6 ms-3'
+    statusPersonagemElement.innerHTML = `${personagem.status} - ${personagem.species}`
 
-    const buttonElement = document.createElement('button');
-    buttonElement.type = 'button';
-    buttonElement.addEventListener('click', () => { modalPersonagem(personagem) });
-    buttonElement.id = 'botao-detalhes';
-    buttonElement.className = 'btn btn-padrao pulsate-fwd active';
-    buttonElement.setAttribute('data-bs-toggle', 'modal');
-    buttonElement.setAttribute('data-bs-target', '#modal-detalhes');
-    buttonElement.style = '--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;';
-    buttonElement.textContent = 'Mais detalhes';
+    const buttonElement = document.createElement('button')
+    buttonElement.type = 'button'
+    buttonElement.addEventListener('click', () => { modalPersonagem(personagem) })
+    buttonElement.id = 'botao-detalhes'
+    buttonElement.className = 'btn btn-padrao pulsate-fwd active'
+    buttonElement.setAttribute('data-bs-toggle', 'modal')
+    buttonElement.setAttribute('data-bs-target', '#modal-detalhes')
+    buttonElement.style = '--bs-btn-padding-y: .25rem --bs-btn-padding-x: .5rem --bs-btn-font-size: .75rem'
+    buttonElement.textContent = 'Mais detalhes'
 
-    cardBodyElement.appendChild(titleElement);
-    statusElement.appendChild(statusPersonagemElement);
-    cardBodyElement.appendChild(statusElement);
-    cardBodyElement.appendChild(buttonElement);
+    cardBodyElement.appendChild(titleElement)
+    statusElement.appendChild(statusPersonagemElement)
+    cardBodyElement.appendChild(statusElement)
+    cardBodyElement.appendChild(buttonElement)
 
-    cardElement.appendChild(imageElement);
-    cardElement.appendChild(cardBodyElement);
+    cardElement.appendChild(imageElement)
+    cardElement.appendChild(cardBodyElement)
 
-    colElemento.appendChild(cardElement);
-    return colElemento;
+    colElemento.appendChild(cardElement)
+    return colElemento
 
 }
 
-//limpa os cards
 function limparElemento(elemento) {
     while (elemento.firstChild) {
-        elemento.removeChild(elemento.firstChild);
+        elemento.removeChild(elemento.firstChild)
     }
 }
 
-//cria a modal com mais informações dos personagens
 function modalPersonagem(personagem) {
     const mostraModalDetalhes = document.getElementById('detalhes')
-    const statusPersonagen = obterIconeStatus(personagem.status);
+    const statusPersonagem = iconeStatus(personagem.status)
 
     const modalContent = `
         <div class="row">
@@ -282,53 +262,49 @@ function modalPersonagem(personagem) {
             <div class="col-6 col-md-7 bg-dark-subtle">
                 <div class="card-body">
                     <h4 class="card-title ">${personagem.name}</h4>
-                    <p class="card-text">${statusPersonagen}<span class="ms-3">${personagem.status}</span> - ${personagem.species}</p>
+                    <p class="card-text">${statusPersonagem}<span class="ms-3">${personagem.status}</span> - ${personagem.species}</p>
                     <h6 class="card-title mb-0 ms-3">Localização:</h6>
                     <p class="card-text ms-3">${personagem.location.name}</p>
                 </div>
             </div>
         </div>
-    `;
-    mostraModalDetalhes.innerHTML = modalContent;
+    `
+    mostraModalDetalhes.innerHTML = modalContent
 }
 
-//mostra o total de pesonagens utilizado no footer
-function mostraTotalPersonagens(quantidade) {
+function totalPersonagens(quantidade) {
     const totalPersonagens = document.getElementById("personagens")
     totalPersonagens.className = 'text-light'
-    totalPersonagens.innerHTML = ` ${quantidade} `;
+    totalPersonagens.innerHTML = ` ${quantidade} `
 
 }
 
-//verifica o status do personagem e cria o circulo redondo com a cor do status(vivo, morto, desconhecido)
-function obterIconeStatus(status) {
+function iconeStatus(status) {
     switch (status) {
         case 'Alive':
-            return '<span class="color_status_vivo"></span>';
+            return '<span class="color_status_vivo"></span>'
         case 'unknown':
-            return '<span class="color_status_desconhecido"></span>';
+            return '<span class="color_status_desconhecido"></span>'
         case 'Dead':
-            return '<span class="color_status_morto"></span>';
+            return '<span class="color_status_morto"></span>'
         default:
-            return '';
+            return ''
     }
 }
 
-//coloca a sombra atrás dos cards conforme o status do pesonagem (vivo, morto, desconhecido)
-function obterSombraStatusCard(status) {
+function sombraStatusCard(status) {
     switch (status) {
         case 'Alive':
-            return 'sombra_card_status_vivo';
+            return 'sombra_card_status_vivo'
         case 'unknown':
-            return 'sombra_card_status_desconhecido';
+            return 'sombra_card_status_desconhecido'
         case 'Dead':
-            return 'sombra_card_status_morto';
+            return 'sombra_card_status_morto'
         default:
-            return '';
+            return ''
     }
 }
 
-//rota para carregar informações sobre os episódios dos funcionários
 async function carregarEpisodios() {
     const episodioEl = document.getElementById("episodios")
     episodioEl.classList = 'text-light'
@@ -337,11 +313,10 @@ async function carregarEpisodios() {
         const response = await api.get(`/episode`)
         episodioEl.innerHTML = ` ${response.data.info.count} `
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
-//rota para carregar a localização dos personagens
-async function carregarLocalizacoes() {
+async function carregarLocalizacao() {
     const localizacaoEl = document.getElementById("localizacoes")
     localizacaoEl.classList = 'text-light'
 
@@ -349,8 +324,8 @@ async function carregarLocalizacoes() {
         const response = await api.get(`/location`)
         localizacaoEl.innerHTML = ` ${response.data.info.count} `
     } catch (error) {
-        console.log(error);
+        console.log(error)
     }
 }
 
-carregamentoInicialPersonagens()
+carregarOsPersonagens()
